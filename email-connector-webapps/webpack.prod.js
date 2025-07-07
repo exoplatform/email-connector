ad5ff19e59d18f7f25f09b2ebb86f9cb67d1d461
Houssem Ben Ali <hbenali.tn@gmail.com>
2025-07-07 12:03:00 +0200

    Init project

---
+++ email-connector-webapps/webpack.prod.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const ESLintPlugin = require('eslint-webpack-plugin');
+const { VueLoaderPlugin } = require('vue-loader')
+
+const config = {
+  context: path.resolve(__dirname, '.'),
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          'babel-loader',
+        ]
+      },
+      {
+        test: /\.vue$/,
+        use: [
+          'vue-loader',
+        ]
+      }
+    ]
+  },
+  entry: {
+  },
+  output: {
+    path: path.join(__dirname, 'target/email-connector/'),
+    filename: 'js/[name].bundle.js',
+    libraryTarget: 'amd'
+  },
+  plugins: [
+    new ESLintPlugin({
+      files: [
+        './src/main/webapp/vue-app/*.js',
+        './src/main/webapp/vue-app/*.vue',
+        './src/main/webapp/vue-app/**/*.js',
+        './src/main/webapp/vue-app/**/*.vue',
+      ],
+    }),
+    new VueLoaderPlugin()
+  ],
+  externals: {
+    vue: 'Vue',
+    vuetify: 'Vuetify',
+    jquery: '$',
+  },
+};
+
+module.exports = config;
