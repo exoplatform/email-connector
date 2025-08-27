60759e6de4572ce7724f8fb80080bb3080493269
Ayoub Zayati <ayoub.ezzayati@exoplatform.com>
2025-08-27 13:03:02 +0200

    feat: Implement email connector administration portlet - EXO-79718 - eXIP-email-connector (#5)

--- email-connector-webapps/webpack.prod.js
+++ email-connector-webapps/webpack.prod.js
@@ -23,6 +23,7 @@ const config = {
     ]
   },
   entry: {
+    emailConnectorAdministration : './src/main/webapp/vue-apps/email-connector-administration/main.js',
   },
   output: {
     path: path.join(__dirname, 'target/email-connector/'),
@@ -32,10 +33,10 @@ const config = {
   plugins: [
     new ESLintPlugin({
       files: [
-        './src/main/webapp/vue-app/*.js',
-        './src/main/webapp/vue-app/*.vue',
-        './src/main/webapp/vue-app/**/*.js',
-        './src/main/webapp/vue-app/**/*.vue',
+        './src/main/webapp/vue-apps/*.js',
+        './src/main/webapp/vue-apps/*.vue',
+        './src/main/webapp/vue-apps/**/*.js',
+        './src/main/webapp/vue-apps/**/*.vue',
       ],
     }),
     new VueLoaderPlugin()
