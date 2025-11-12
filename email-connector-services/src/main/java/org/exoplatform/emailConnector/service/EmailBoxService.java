70adad0a1690097229d283bff8f6c378387618e0
Ayoub Zayati <ayoub.ezzayati@exoplatform.com>
2025-11-12 14:55:28 +0100

    feat: Mark as unread email from email detail - EXO-82163 - exoplatform/eXIP#27 (#71)

--- email-connector-services/src/main/java/org/exoplatform/emailConnector/service/EmailBoxService.java
+++ email-connector-services/src/main/java/org/exoplatform/emailConnector/service/EmailBoxService.java
@@ -270,8 +270,8 @@ public class EmailBoxService {
     }
   }
 
-  public Email getEmailByMailRemoteIdAndUserId(String userId, long mailRemoteId) {
-    return emailBoxStorage.getEmailByMailRemoteIdAndUserId(userId, mailRemoteId);
+  public Email getEmailByMailRemoteIdAndUserId(long mailRemoteId, String userName) {
+    return emailBoxStorage.getEmailByMailRemoteIdAndUserId(userName, mailRemoteId);
   }
 
   public void updateEmailReadStatus(long emailRemoteId,
@@ -279,8 +279,8 @@ public class EmailBoxService {
                                     String username,
                                     boolean readStatus,
                                     boolean updateRemoteReadStatus) throws IllegalAccessException {
-    Email email = getEmailByMailRemoteIdAndUserId(username, emailRemoteId);
-    if (readStatus != email.isRead()) {
+    Email email = getEmailByMailRemoteIdAndUserId(emailRemoteId, username);
+    if (email != null && readStatus != email.isRead()) {
       email.setRead(readStatus);
       emailBoxStorage.updateEmail(email);
       if (updateRemoteReadStatus) {
@@ -297,7 +297,9 @@ public class EmailBoxService {
             inbox = store.getFolder("INBOX");
             inbox.open(Folder.READ_WRITE);
             remoteMessage = ((UIDFolder) inbox).getMessageByUID(emailRemoteId);
-            remoteMessage.setFlag(Flags.Flag.SEEN, readStatus);
+            if (remoteMessage != null) {
+              remoteMessage.setFlag(Flags.Flag.SEEN, readStatus);
+            }
           } catch (Exception e) {
             LOG.error("Error when connecting store for user {}", username, e);
             throw new IllegalStateException(String.format("Error when connecting store for user %s", username));
@@ -335,7 +337,7 @@ public class EmailBoxService {
                                                                                             IllegalAccessException {
     for (Message message : serverMessages) {
       long messageUid = uidFolder.getUID(message);
-      Email email = getEmailByMailRemoteIdAndUserId(username, messageUid);
+      Email email = getEmailByMailRemoteIdAndUserId(messageUid, username);
       if (email == null) {
         try {
           String excerpt = EmailConnectorUtils.getMessageContent(new MimeMessage((MimeMessage) message), true);
